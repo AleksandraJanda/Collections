fa019a4f7b35600446db63c7c4fca6718bdcf8c5
Aleksandra Janda <aleksandra_janda@wp.pl>
2018-10-21 09:46:48 +0200

    fibonacci update commit

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,5 +1,17 @@
 public class Fibonacci {
+    public static void fibonacci(){
+        int[] array = new int[10];
+        array[0]=0;
+        array[1]=1;
+        for(int i=2;i<array.length;i++){
+            array[i]=array[i-1]+array[i-2];
+        }
+        for (int x:array) {
+            System.out.print(x+"\t");
+        }
+    }
+
     public static void main(String[] args) {
-        
+        fibonacci();
     }
 }

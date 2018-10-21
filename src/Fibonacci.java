59564e2295b8e672aca25e07ec5d9d23243ce2fc
Aleksandra Janda <aleksandra_janda@wp.pl>
2018-10-21 10:38:02 +0200

    fibonacci recursion update

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,4 +1,5 @@
 public class Fibonacci {
+    private int sum;
     public static void fibonacci(){
         int[] array = new int[10];
         array[0]=0;
@@ -10,8 +11,22 @@ public class Fibonacci {
             System.out.print(x+"\t");
         }
     }
-
+    public int fibonacciRekurencja(int n){
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }else{
+            return fibonacciRekurencja(n-1)+fibonacciRekurencja(n-2);
+        }
+    }
     public static void main(String[] args) {
         fibonacci();
+        System.out.println();
+
+        Fibonacci fibonacci = new Fibonacci();
+        for(int i=0;i<10;i++){
+            System.out.print(fibonacci.fibonacciRekurencja(i)+"\t");
+        }
     }
 }
